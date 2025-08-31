9f72d55022bee37a2ee1b5f77816d491251efd20
minh2995-debug <minh2995@gmail.com>
2025-08-31 18:11:36 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -307,7 +307,7 @@ export default function EmployeeFeedbackApp() {
 
     // 2) Optional: send to your backend / Google Apps Script (uncomment & set URL)
     try {
-       await fetch("https://script.google.com/macros/s/AKfycby0ty1vB34djARChGgmzIVrMuE5RZqgaSLOsU7mXe9LNmifAdFfYDH1ojptIERiW69T/exec", {
+       await fetch("https://script.google.com/macros/s/AKfycbxuu3RVGg2KB-FszIJAieFwHRdPIUhzus8v1n7qQeVtXblaZna80rDQ-npzU8YsUFjk/exec", {
          method: "POST",
          headers: { "Content-Type": "application/json" },
          body: JSON.stringify(payload),
